48ccd802a574240e84b409ad04b7b5c55e771e41
ben y <benyager36@gmail.com>
2020-03-10 17:58:45 +0200

    Updates

---
+++ precache-manifest.4570f85f2b53ae81809aac4eadbfb0f0.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37834dd3254bc65edcbabeb0a5330a17",
+    "url": "/to-do-project/index.html"
+  },
+  {
+    "revision": "ce96a7562c94504e124f",
+    "url": "/to-do-project/static/css/2.3bf270e6.chunk.css"
+  },
+  {
+    "revision": "f824019cb9e96e8de5f8",
+    "url": "/to-do-project/static/css/main.6aff740b.chunk.css"
+  },
+  {
+    "revision": "ce96a7562c94504e124f",
+    "url": "/to-do-project/static/js/2.0df6b361.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/to-do-project/static/js/2.0df6b361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f824019cb9e96e8de5f8",
+    "url": "/to-do-project/static/js/main.9b9e06ae.chunk.js"
+  },
+  {
+    "revision": "cc79ee6ab9d3d1b78e0c",
+    "url": "/to-do-project/static/js/runtime-main.ead9d3ba.js"
+  },
+  {
+    "revision": "0841af952c807bdf56455b1addb4c7df",
+    "url": "/to-do-project/static/media/IndieFlower-Regular.0841af95.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/to-do-project/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/to-do-project/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/to-do-project/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/to-do-project/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/to-do-project/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/to-do-project/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/to-do-project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/to-do-project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/to-do-project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/to-do-project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/to-do-project/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/to-do-project/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/to-do-project/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/to-do-project/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/to-do-project/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/to-do-project/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/to-do-project/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/to-do-project/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/to-do-project/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/to-do-project/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/to-do-project/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
